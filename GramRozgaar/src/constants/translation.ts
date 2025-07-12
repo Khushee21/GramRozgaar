42c798b52e7b345b3f92aedc343f72683891d440
Khushi Rathore <khushirathore649@gmail.com>
2025-07-13 00:53:58 +0530

    changes done

---
+++ GramRozgaar/src/constants/translation.ts
@@ -0,0 +1,31 @@
+// constants/translations.ts
+export const translations = {
+    en: {
+        signUp: "Sign Up",
+        signIn: "Sign In",
+        name: "Name",
+        age: "Age",
+        village: "Village",
+        phone: "Phone Number",
+        password: "Password",
+        confirmPassword: "Confirm Password",
+        register: "Register",
+        login: "Login",
+        toggleToSignIn: "Already have an account? Sign In",
+        toggleToSignUp: "Don't have an account? Sign Up",
+    },
+    hi: {
+        signUp: "रजिस्टर करें",
+        signIn: "साइन इन करें",
+        name: "नाम",
+        age: "उम्र",
+        village: "गांव",
+        phone: "फोन नंबर",
+        password: "पासवर्ड",
+        confirmPassword: "पासवर्ड की पुष्टि करें",
+        register: "रजिस्टर करें",
+        login: "लॉग इन करें",
+        toggleToSignIn: "पहले से खाता है? साइन इन करें",
+        toggleToSignUp: "कोई खाता नहीं है? रजिस्टर करें",
+    }
+};
