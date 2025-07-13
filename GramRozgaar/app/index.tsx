d24f83afe4bae4794e5b5beb0d0608704bc405bb
Khushi Rathore <khushirathore649@gmail.com>
2025-07-13 18:04:11 +0530

    testing

---
+++ GramRozgaar/app/index.tsx
@@ -0,0 +1,11 @@
+// app/index.tsx
+import { View, Text } from "react-native";
+import LoginRegistrtaion from "./auth/LoginRegistrationScreen";
+
+export default function Home() {
+    return (
+        < View>
+            <LoginRegistrtaion />
+        </View >
+    );
+}
